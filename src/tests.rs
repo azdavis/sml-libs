360d865bfe1e8afc4f8e483e0ac8f53da0593041
Ariel Davis <ariel.z.davis@icloud.com>
2023-04-07 03:10:48 -0700

    Rm xshell

--- src/tests.rs
+++ src/tests.rs
@@ -1,6 +1,4 @@
 use std::collections::BTreeSet;
-use std::path::Path;
-use xshell::Shell;
 
 fn eq_sets<T>(lhs: &BTreeSet<T>, rhs: &BTreeSet<T>, only_lhs: &str, only_rhs: &str)
 where
@@ -14,35 +12,28 @@ where
 
 #[test]
 fn all_used() {
-  let sh = Shell::new().unwrap();
-  sh.change_dir(env!("CARGO_MANIFEST_DIR"));
-  let mut path = Path::new("src").to_owned();
-  let mut entries = sh.read_dir(&path).unwrap();
-  entries.retain(|x| x.extension().is_none());
+  std::env::set_current_dir(env!("CARGO_MANIFEST_DIR")).unwrap();
+  let entries = std::fs::read_dir("src").unwrap();
   for dir in entries {
-    let dir_name = dir
-      .file_name()
-      .expect("no file name for dir")
-      .to_str()
-      .expect("cannot convert dirname to str");
-    let mod_path = dir.as_path().with_extension("rs");
-    path.push(mod_path);
-    let mod_file = sh.read_file(&path).unwrap();
+    let dir = dir.unwrap();
+    if dir.path().extension().is_some() {
+      continue;
+    }
+    let dir_name = dir.file_name().into_string().expect("cannot convert dirname to str");
+    let mod_path = dir.path().with_extension("rs");
+    let mod_file = std::fs::read_to_string(&mod_path).unwrap();
     let prefix = format!("  \"{dir_name}/");
     let in_order: BTreeSet<_> =
       mod_file.lines().filter_map(|x| x.strip_prefix(&prefix)?.strip_suffix(".sml\",")).collect();
-    assert!(path.pop());
-    path.push(&dir);
-    let sml_dir = sh.read_dir(&path).unwrap();
-    let in_files: BTreeSet<_> = sml_dir
-      .iter()
-      .map(|x| x.file_name().unwrap().to_str().unwrap().strip_suffix(".sml").unwrap())
-      .collect();
+    let sml_dir = std::fs::read_dir(dir.path()).unwrap();
+    let in_files: BTreeSet<_> =
+      sml_dir.map(|x| x.unwrap().file_name().into_string().unwrap()).collect();
+    let in_files: BTreeSet<_> = in_files.iter().map(|x| x.strip_suffix(".sml").unwrap()).collect();
     eq_sets(
       &in_order,
       &in_files,
-      "referenced files that don't exist",
-      "existing files not referenced",
+      &format!("{}: referenced files that don't exist", dir.path().display()),
+      &format!("{}: existing files not referenced", dir.path().display()),
     );
   }
 }
